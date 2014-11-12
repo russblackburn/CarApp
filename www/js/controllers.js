b64fe70b6da2c38a74a5b7cac61de86f98797c53
Russ Blackburn <russ.c.blackburn@gmail.com>
2014-11-12 11:23:38 -0700

    adding vin scanner function

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,13 +7,20 @@ angular.module('starter.controllers', [])
 .controller('CarsCtrl', function($scope, Cars, $cordovaBarcodeScanner) {
   $scope.cars = Cars.all();
         $scope.orderProp = '-year';
-
         //adding barcode scanner
         $scope.scanBarcode = function() {
             $cordovaBarcodeScanner.scan().then(function(imageData) {
-                alert(imageData.text);
-                console.log("Barcode Format -> " + imageData.format);
-                console.log("Cancelled -> " + imageData.cancelled);
+
+                $scope.barcoderesults = imageData.text;
+                document.getElementById("scannerInput").value = imageData.text;
+
+                if (imageData.cancelled == 1) {
+                    alert("Cancelled Scan");
+                } else {
+                    console.log("Scan Not Cancelled ->" + imageData.cancelled);
+                }
+                //console.log("Barcode Format -> " + imageData.format);
+                //console.log("Cancelled -> " + imageData.cancelled);
             }, function(error) {
                 console.log("An error happened -> " + error);
             });
